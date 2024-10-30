6cd1d7ff8a8d83721eb45bbff49b39700b184859
raselworshop <raselworshop@gmail.com>
2024-10-30 12:35:29 -0400

    reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 
 import './App.css'
-import CustomHooksForm from './Components/CustomHookForm/CustomHooksForm'
+import ReusableForm from './Components/ReusableComponent/ReusableForm'
+// import CustomHooksForm from './Components/CustomHookForm/CustomHooksForm'
 // import RefForm from './Components/RefForm/RefForm'
 // import StateFullForm from './Components/StateFullForm/StateFullForm'
 // import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 function App() {
 
+  const handleSignUpSubmit = data => {
+    console.log('Sign up data', data)
+  }
+  const handleProfileUpdateSubmit = data => {
+    console.log('Update profile data', data)
+  }
+
   return (
     <>
-     
+
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFullForm></StateFullForm> */}
       {/* <RefForm></RefForm> */}
-      <CustomHooksForm></CustomHooksForm>
+      {/* <CustomHooksForm></CustomHooksForm> */}
+      <ReusableForm formTitle={'Sign Up'} handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h3>Sign Up</h3>
+          <p>Please Sign Up Right Now!!</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm formTitle={'Profile Update'} handleSubmit={handleProfileUpdateSubmit} submitBtnTxt={'Update'}>
+        <div>
+          <h3>Profile Update</h3>
+          <p>Please Keep your profile up to date!!</p>
+        </div>
+      </ReusableForm>
     </>
   )
 }
---
+++ src/Components/ReusableComponent/ReusableForm.jsx
@@ -0,0 +1,26 @@
+
+const ReusableForm = ({formTitle, handleSubmit, submitBtnTxt='Submit', children}) => {
+    const handleLocalSubmit = e =>{
+        e.preventDefault();
+        const data = {
+            name:e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value,
+        }
+        handleSubmit(data)
+    }
+    return (
+        <div>
+            {/* <h2>{formTitle}</h2> insted use next one */}
+            {children}
+            <form onSubmit={handleLocalSubmit}>
+                <input type="text" name="name"/><br />
+                <input type="email" name="email" id="" /><br />
+                <input type="password" name="password" id="" /><br />
+                <input type="submit" value={submitBtnTxt} />
+            </form>
+        </div>
+    );
+};
+
+export default ReusableForm;
