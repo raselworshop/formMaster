09b4c8cf14f0d982fad611ba1119bca37886503f
raselworshop <raselworshop@gmail.com>
2024-10-29 21:52:53 -0400

    added the master form system

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import StateFullForm from './Components/StateFullForm/StateFullForm'
+// import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      <StateFullForm></StateFullForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/StateFullForm/StateFullForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const StateFullForm = () => {
+    const [name, setName] = useState(null)
+    const [email, setEmail] = useState(null);
+    const [password, setPassword]= useState(null)
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(name, email, password)
+    }
+
+    const handleNameChange = e =>{
+        console.log(e.target.value)
+        setName(e.target.value)
+    }
+    const handleEmailChange = e =>{
+        console.log(e.target.value)
+        const values = e.target.value;
+        setEmail(values)
+    }
+    const handlePasswordChange = e =>{
+        console.log(e.target.value)
+        setPassword(e.target.value)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleNameChange}
+                 type="text" name="name"/><br />
+                <input onChange={handleEmailChange}
+                 type="email" name="email" id="" /><br />
+                <input type="text" name="phone" id="" /><br />
+                <input onChange={handlePasswordChange}
+                type="password" name="password" id="" required/><br />
+                <input type="submit" value='submit' />
+            </form>
+        </div>
+    );
+};
+
+export default StateFullForm;
